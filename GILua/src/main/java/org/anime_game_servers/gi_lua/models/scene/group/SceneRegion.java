8650f6c7f2be5337becb563cea035eaf46a2d8db
Nazrin <lewasite@yahoo.com>
2024-09-25 07:40:38 -0700

    Implement ScriptRegionShape.POLYGON and ScriptRegionShape.CYLINDER (#33)

--- GILua/src/main/java/org/anime_game_servers/gi_lua/models/scene/group/SceneRegion.java
+++ GILua/src/main/java/org/anime_game_servers/gi_lua/models/scene/group/SceneRegion.java
@@ -35,16 +35,47 @@ public class SceneRegion extends SceneObject{
 
     public boolean contains(Vector position) {
         switch (shape) {
-            case ScriptRegionShape.CUBIC:
-                return (Math.abs(pos.getX() - position.getX()) <= size.getX()/2f) &&
-                       (Math.abs(pos.getY() - position.getY()) <= size.getY()/2f) &&
-                       (Math.abs(pos.getZ() - position.getZ()) <= size.getZ()/2f);
-            case ScriptRegionShape.SPHERE:
-                var x = Math.pow(pos.getX() - position.getX(), 2);
-                var y = Math.pow(pos.getY() - position.getY(), 2);
-                var z = Math.pow(pos.getZ() - position.getZ(), 2);
-                // ^ means XOR in java!
-                return x + y + z <= (radius*radius);
+            case ScriptRegionShape.SPHERE -> {
+                var x = pos.getX() - position.getX();
+                var y = pos.getY() - position.getY();
+                var z = pos.getZ() - position.getZ();
+                // x^2 + y^2 + z^2 = radius^2
+                return x * x + y * y + z * z <= (radius * radius);
+            }
+            case ScriptRegionShape.CUBIC -> {
+                return (Math.abs(pos.getX() - position.getX()) <= size.getX() / 2f) &&
+                       (Math.abs(pos.getY() - position.getY()) <= size.getY() / 2f) &&
+                       (Math.abs(pos.getZ() - position.getZ()) <= size.getZ() / 2f);
+            }
+            case ScriptRegionShape.POLYGON -> {
+                // algorithm is "ray casting": https://www.youtube.com/watch?v=RSXM9bgqxJM
+                if (Math.abs(pos.getY() - position.getY()) > height / 2f) return false;
+                var count = 0;
+                for (var i = 0; i < pointArray.size(); ++i) {
+                    var j = (i + 1) % pointArray.size();
+
+                    var yp = position.getZ();
+                    var y1 = pointArray.get(i).getY();
+                    var y2 = pointArray.get(j).getY();
+
+                    var xp = position.getX();
+                    var x1 = pointArray.get(i).getX();
+                    var x2 = pointArray.get(j).getX();
+
+                    if ((yp < y1) != (yp < y2)
+                            && xp < x1 + ((yp - y1) / (y2 - y1)) * (x2 - x1)) {
+                        ++count;
+                    }
+                }
+                return count % 2 == 1;
+            }
+            case ScriptRegionShape.CYLINDER -> {
+                if (Math.abs(pos.getY() - position.getY()) > height / 2f) return false;
+                var x = pos.getX() - position.getX();
+                var z = pos.getZ() - position.getZ();
+                // x^2 + z^2 = radius^2
+                return x * x + z * z <= radius * radius;
+            }
         }
         return false;
     }
